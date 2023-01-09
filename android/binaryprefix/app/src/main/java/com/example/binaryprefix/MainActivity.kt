8d4d36e44c16f571a565d7abca3c4c6e9a1ccc0e
Dr. Paween <frxprince@gmail.com>
2023-01-09 14:37:51 +0700

    morning break

--- android/binaryprefix/app/src/main/java/com/example/binaryprefix/MainActivity.kt
+++ android/binaryprefix/app/src/main/java/com/example/binaryprefix/MainActivity.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -15,7 +16,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var byteValue:Double
+        var byteValue:Double=0.0
         var btn_inputprefix =findViewById<Button>(R.id.button_inputprefix)
         var textinput=findViewById<EditText>(R.id.editTextNumberDecimal)
         var textoutput=findViewById<TextView>(R.id.textView2)
@@ -40,9 +41,25 @@ class MainActivity : AppCompatActivity() {
            var i = Intent(this,InputPrefix::class.java)
           InputPrefixResult.launch(i)        //startActivity(i)
        }
+       var TargetPrefixResult=registerForActivityResult(
+           ActivityResultContracts.StartActivityForResult())
+       { result:ActivityResult->if(result.resultCode== RESULT_OK)
+       {
+        var ans=when(result.data!!.getStringExtra("prefix")){
+         "kiB"->(byteValue/Math.pow(2.0,10.0)).toString()+" kiB"
+         "MiB"->(byteValue/Math.pow(2.0,20.0)).toString()+" MiB"
+         "GiB"->(byteValue/Math.pow(2.0,30.0)).toString()+" GiB"
+            "TiB"->(byteValue/Math.pow(2.0,40.0)).toString()+" TiB"
+            else->"i don't know"
+        }
+        var i=Intent(this,ShowResult::class.java)
+           i.putExtra("result",ans)
+           startActivity(i)
+       }else{}
+       }
         btn_convert.setOnClickListener {
             var i = Intent(this,TargetPrefix::class.java)
-            startActivity(i)
+         TargetPrefixResult.launch(i)
         }
     }
 }
